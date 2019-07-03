004368eed91951497265f08e79c9d6f460fb4065
unknown <hoangvannhien197@gmai.com>
2019-07-03 10:57:23 +0700

    pagination(phan trang)

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,8 +4,20 @@ module.exports.index = function(req, res) {
 	var perPage = 8; //x
 	var start = (page - 1)*perPage;
 	var end = page*perPage;
+
+	var pagIndex = db.get('products').value().length
+	console.log(pagIndex)
+	var page = Math.ceil(pagIndex / perPage)
+
+	var p = []
+
+	for (var i = 1; i <= page; i++){
+		p.push(i)
+	}
+
 	res.render('products/index',{
-		products:db.get('products').value().slice(start,end)
+		products:db.get('products').value().slice(start,end),
+		page: p
 	});
 	
 };
